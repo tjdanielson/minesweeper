51326521b0036fc25a1928cfecd053949f51548e
Tessa Danielson <tessajdanielson@gmail.com>
2018-01-07 15:06:13 -0600

    Add part 3 of project.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,48 @@
-const printBoard = board => {
-  console.log('current board: ');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
 
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+
+  let numberOfBombsPlaced = 0;
+
+  while (numberOfBombsPlaced < numberOfBombs) {
+    // has potential to place bombs on top of bombs. will fix after learning control flow
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++
+  }
+
+  return board;
+};
+
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ')
+printBoard(bombBoard);
